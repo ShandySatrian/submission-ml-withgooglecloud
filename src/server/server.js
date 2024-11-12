6aa35eb6431b30e1c3ab9664b15e9dd527b41950
Shandy Satria Nugraha <shandysatrian@gmail.com>
2024-11-13 00:38:56 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,8 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
 	const server = Hapi.server({
-		port: process.env.PORT || 8080,
+		port: process.env.APP_PORT || 8080,
+		host: process.env.APP_HOST || "localhost",
 		routes: {
 			cors: {
 				origin: ["*"],
